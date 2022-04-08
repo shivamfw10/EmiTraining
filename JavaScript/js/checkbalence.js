30b71933800e77595e3872c2c89c5de9f91b1f49
Shivam Vishvakarma <Shivam.Vishvakarma@euromonitor.com>
2022-04-08 14:13:33 +0530

    functionality done

---
+++ JavaScript/js/checkbalence.js
@@ -0,0 +1,31 @@
+
+const checkBalence=(event)=>{
+    event.preventDefault();
+    var dataDiv = document.getElementById("viewData");
+    dataDiv.hidden=false;
+    var accNumber = document.getElementById("acNumber");
+    var accType = document.getElementById("acType");
+    var accName = document.getElementById("acName");
+    var accBalence = document.getElementById("acBalence");
+    const name = document.getElementById("accountNumber").value;
+    const emiuser = JSON.parse(localStorage.getItem("emiuser"));
+    for(let i=0;i<emiuser.length;i++){
+        let ac_number = emiuser[i].accountNumber;
+        if(ac_number==name){
+            accNumber.innerHTML=`${emiuser[i].accountNumber}`;
+            accType.innerHTML = `${emiuser[i].accountType}`;
+            accName.innerHTML = `${emiuser[i].firstName} ${emiuser[i].lastName}`;
+            accBalence.innerHTML = `${emiuser[i].balence}`;
+            break;
+        }
+        else
+        {
+            alert("Enter valid account number/name");
+            clear();
+        }
+    }
+}
+
+const clear=()=>{
+    document.getElementById("name").value=null;
+}
