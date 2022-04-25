7f7b38072b15643ba685bf28f178c369b405c7fc
Venu Gopal <venugopal.kundella@euromonitor.com>
2022-04-25 16:07:33 +0530

    array done

---
+++ TypeScript/annotation/array.ts
@@ -0,0 +1,39 @@
+// const carMakers:string[] = ['ford','toyota','chevy'];
+
+// const dates = [new Date(), new Date()];
+
+// const carsByMake:string[][]=[
+//     ['f150'],
+//     ['corolla'],
+//     ['camera']
+// ];
+
+// const carsManufacture:string[][]=[];
+
+//Help with inference when extracting values
+
+
+//const car = carMakers[0];
+
+//const myCar = carMakers.pop();
+
+
+//Prevent incompaitible value
+//carMakers.push(100);
+
+
+//Help with 'map'
+
+// carMakers.map((car:string):string=>{
+//     return car.toUpperCase();
+// });
+
+// console.log(carMakers);
+
+
+//Flexible types
+
+//const importantDate:(Date|string)[] = [new Date()];
+// importantDate.push('2022-05-05');
+// importantDate.push(new Date());
+// console.log(importantDate);
