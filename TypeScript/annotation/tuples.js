1c9e6c573b732f56e639674536c45eb7001d63d4
Venu Gopal <venugopal.kundella@euromonitor.com>
2022-04-25 16:09:09 +0530

    tuples done

---
+++ TypeScript/annotation/tuples.js
@@ -0,0 +1,16 @@
+var drink = {
+    color: 'brown',
+    carbonated: true,
+    sugar: 40
+};
+var pepsi = ['brown', true, 40];
+var thumsup = ['black', true, 30];
+var tea = ['brown', false, 0];
+// pepsi[0]=40;
+// pepsi[2]='brown'
+var carSpecs = [400, 3354];
+var carStats = {
+    horsepower: 400,
+    weight: 3354
+};
+console.log(carStats);
---
+++ TypeScript/annotation/tuples.ts
@@ -0,0 +1,22 @@
+const drink ={
+    color:'brown',
+    carbonated:true,
+    sugar:40
+};
+type Drink = [string,boolean,number]
+
+const pepsi:[string,boolean,number] = ['brown',true,40];
+const thumsup:Drink=['black',true,30];
+const tea:Drink=['brown',false,0];
+
+// pepsi[0]=40;
+// pepsi[2]='brown'
+
+const carSpecs:[number,number]=[400,3354];
+
+const carStats ={
+    horsepower:400,
+    weight:3354
+};
+
+console.log(carStats);
