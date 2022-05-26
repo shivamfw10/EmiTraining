5b8d483ee03c28636d3edb485bc097cf814d4873
Shivam Vishvakarma <vishvashivam32@gmail.com>
2022-05-26 16:58:50 +0530

    nested routing done

--- AngularApp/src/app/app-routing.module.ts
+++ AngularApp/src/app/app-routing.module.ts
@@ -2,6 +2,12 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { AboutusComponent } from './component/aboutus/aboutus.component';
 import { ContactusComponent } from './component/contactus/contactus.component';
+import { DataBindingComponent } from './component/data-binding/data-binding.component';
+import { EmployeeAddComponent } from './component/employee-add/employee-add.component';
+import { EmployeeComponent } from './component/employee/employee.component';
+import { EmployeeDeleteComponent } from './component/employee-delete/employee-delete.component';
+import { EmployeeDetailComponent } from './component/employee-detail/employee-detail.component';
+import { EmployeeEditComponent } from './component/employee-edit/employee-edit.component';
 import { GalleryComponent } from './component/gallery/gallery.component';
 import { HomeComponent } from './component/home/home.component';
 import { NgModule } from '@angular/core';
@@ -16,7 +22,15 @@ const routes: Routes = [
   {path:"gallery",component:GalleryComponent},
   {path:"services",component:ServicesComponent},
   {path:"contactus",component:ContactusComponent},
+  {path:"databinding",component:DataBindingComponent},
   {path:"training",component:TrainingComponent},
+  {path:"employee",component:EmployeeComponent,children:[
+    {path:"details",component:EmployeeDetailComponent,children:[
+      {path:"add",component:EmployeeAddComponent},
+      {path:"delete",component:EmployeeDeleteComponent},
+      {path:"update",component:EmployeeEditComponent}
+    ]},
+  ]},
   {path:"**",component:PageNotFoundComponent}
 ];
 
--- AngularApp/src/app/app.module.ts
+++ AngularApp/src/app/app.module.ts
@@ -4,13 +4,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { ContactusComponent } from './component/contactus/contactus.component';
 import { DataBindingComponent } from './component/data-binding/data-binding.component';
+import { EmployeeAddComponent } from './component/employee-add/employee-add.component';
+import { EmployeeComponent } from './component/employee/employee.component';
+import { EmployeeDeleteComponent } from './component/employee-delete/employee-delete.component';
+import { EmployeeDetailComponent } from './component/employee-detail/employee-detail.component';
+import { EmployeeEditComponent } from './component/employee-edit/employee-edit.component';
 import { FormsModule } from '@angular/forms';
 import { GalleryComponent } from './component/gallery/gallery.component';
 import { HomeComponent } from './component/home/home.component';
+import { MenuComponent } from './component/menu/menu.component';
 import { NgModule } from '@angular/core';
 import { PageNotFoundComponent } from './component/page-not-found/page-not-found.component';
 import { ServicesComponent } from './component/services/services.component';
-import { MenuComponent } from './component/menu/menu.component';
 import { TrainingComponent } from './component/training/training.component';
 
 @NgModule({
@@ -24,7 +29,12 @@ import { TrainingComponent } from './component/training/training.component';
     PageNotFoundComponent,
     DataBindingComponent,
     MenuComponent,
-    TrainingComponent
+    TrainingComponent,
+    EmployeeDetailComponent,
+    EmployeeEditComponent,
+    EmployeeDeleteComponent,
+    EmployeeAddComponent,
+    EmployeeComponent
   ],
   imports: [
     BrowserModule,
