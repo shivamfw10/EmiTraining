f45eb02e57ed2fc4b5a5175cc0eb97686d4338d0
shivamfw10 <akgeeta8874@gmail.com>
2022-06-08 16:38:24 +0530

    done

--- CustomDirectives/src/app/highlight.directive.ts
+++ CustomDirectives/src/app/highlight.directive.ts
@@ -3,25 +3,30 @@ import { Directive, ElementRef, HostListener, Input } from '@angular/core';
 @Directive({
   selector: '[appHighlight]'
 })
-
+ 
 export class HighlightDirective {
 
   @Input() appHighlight='';
-  @Input() defaultColor='';
+  @Input() appFontWeight='';
+  @Input() appTextSize='';
+  @Input() appBgColor='';
   constructor(private el:ElementRef) {
-    this.el.nativeElement.style.backgroundColor ='yellow';
+   // this.el.nativeElement.style.backgroundColor ='yellow';
+    this.el.nativeElement.style.fontSize='';
    }
    @HostListener('mouseenter') onMouseEnter() {
-    this.highlight(this.appHighlight || this.defaultColor || 'red');
-
+    this.highlight(this.appHighlight='red',this.appTextSize='40px',this.appBgColor='yellow',this.appFontWeight='700');
   }
   
   @HostListener('mouseleave') onMouseLeave() {
-    this.highlight('');
+    this.highlight('','','','');
   }
   
-  private highlight(color: string) {
-    this.el.nativeElement.style.backgroundColor = color;
+  private highlight(color: string,textSize:string,bgColor:string,fontWeight:string) {
+    this.el.nativeElement.style.color = color;
+    this.el.nativeElement.style.fontSize=textSize;
+    this.el.nativeElement.style.backgroundColor=bgColor;
+    this.el.nativeElement.style.fontWeight=this.appFontWeight;
   }
 
 }
