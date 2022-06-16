e2840c6d8e95f44617b6f42a2ff491736fe5c208
shivamfw10 <akgeeta8874@gmail.com>
2022-06-16 11:20:36 +0530

    Component Life Cylce Done

---
+++ ComponentLifeCycle/src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'ComponentLifeCycle';
+  inputText:string='';
+  destroy:boolean=true;
+  OnSubmit(inputEl:HTMLInputElement){
+    this.inputText=inputEl.value;
+  }
+
+  DestroyComponent(){
+    this.destroy=false;
+  }
+}
---
+++ ComponentLifeCycle/src/app/demo/demo.component.ts
@@ -0,0 +1,48 @@
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, Input, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
+
+@Component({
+  selector: 'app-demo',
+  templateUrl: './demo.component.html',
+  styleUrls: ['./demo.component.css']
+})
+export class DemoComponent implements OnInit, OnChanges, 
+DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit,
+AfterViewChecked, OnDestroy {
+  @Input() value:string='Shivam';
+  constructor() { 
+    console.log("Constructor called!");
+    console.log(this.value);
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log("ngOnChanges called!");
+    console.log(changes);
+  }
+  ngOnInit(): void {
+    console.log("ngOnInit called!");
+    console.log(this.value);
+  }
+
+  ngDoCheck(): void {
+    console.log("ngDoCheck called!");
+  }
+
+  ngAfterContentInit(): void {
+    console.log("ngAfterContentInit called!");
+  }
+
+  ngAfterContentChecked(): void {
+    console.log("ngAfterContentChecked called!");
+  }
+
+  ngAfterViewInit(): void {
+    console.log("ngAfterViewInit called!");
+  }
+
+  ngAfterViewChecked(): void {
+    console.log("ngAfterViewChecked called!");
+  }
+
+  ngOnDestroy(): void {
+    console.log("ngOnDestroy called!");
+  }
+}
