5d1cfa238c076ce6e4e7d1b4fc3389ad51754821
shivamfw10 <akgeeta8874@gmail.com>
2022-06-19 09:32:43 +0530

    Template Driven Form

--- Assignments/AngularForm/src/app/app.component.ts
+++ Assignments/AngularForm/src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,57 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AngularForm';
+  defaultCountry = 'india';
+
+  firstname:string | undefined;
+  lastname:string | undefined;
+  email: string | undefined;
+  gen:string | undefined;
+  country: string | undefined;
+
+  defaultGender = 'Female';
+
+  gender = [
+    {id: '1', value: 'Male'},
+    {id: '2', value: 'Female'},
+    {id: '3', value: 'Other'}
+  ]
+  
+  @ViewChild('myForm') form!: NgForm;
+
+  onSubmit(){
+    console.log(this.form);
+
+    this.firstname = this.form.value.personDetails.firstname;
+    this.lastname = this.form.value.personDetails.lastname;
+    this.email = this.form.value.personDetails.email;
+    this.gen = this.form.value.gender;
+    this.country = this.form.value.country;
+
+    this.form.reset();
+  }
+
+  setDefaultValues(){
+    // this.form.value.personDetails.firstname = 'John';
+    // this.form.value.personDetails.lastname = 'smith';
+    // this.form.value.personDetails.email = 'abc@example.com';
+    // this.form.setValue({
+    //   country: '',
+    //   gender: '',
+    //   hobbies: '',
+    //   personDetails: {
+    //     firstname: 'John',
+    //     lastname: 'Smith',
+    //     email: 'abc@example.com'
+    //   }
+    // })
+
+    this.form.form.patchValue({
+      personDetails: {
+         firstname: 'John',
+         lastname: 'Smith',
+         email: 'abc@example.com'
+      }
+    })
+  }
 }
