65d6eeefefadd4bfa4a843ac664fb4471bbfe67f
shivamfw10 <akgeeta8874@gmail.com>
2022-06-24 08:53:14 +0530

    form done

--- Assignments/AngularMaterial/src/app/app.module.ts
+++ Assignments/AngularMaterial/src/app/app.module.ts
@@ -7,6 +7,7 @@ import {MatButtonModule} from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import {MatTableModule} from '@angular/material/table';
 import {MatToolbarModule} from '@angular/material/toolbar';
@@ -31,7 +32,8 @@ import { ResitrationFormComponent } from './resitration-form/resitration-form.co
     MatTableModule,
     MatPaginatorModule,
     MatDialogModule,
-    MatFormFieldModule
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
