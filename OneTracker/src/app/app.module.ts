524a730270f93663a82966f40674d630a36d2faf
shivamfw10 <akgeeta8874@gmail.com>
2022-06-26 23:04:28 +0530

    header and login UI

--- OneTracker/src/app/app.module.ts
+++ OneTracker/src/app/app.module.ts
@@ -1,18 +1,24 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { LoginComponent } from './login/login.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { LandingPageComponent } from './landing-page/landing-page.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { AddTicketComponent } from './ticket/add-ticket/add-ticket.component';
-import { ViewTicketComponent } from './ticket/view-ticket/view-ticket.component';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
+import { DashboardComponent } from './dashboard/dashboard.component';
 import { EditTicketComponent } from './ticket/edit-ticket/edit-ticket.component';
 import { GenerateReportComponent } from './generate-report/generate-report.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { LoginComponent } from './login/login.component';
+import {MatButtonModule} from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatGridListModule} from '@angular/material/grid-list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import {MatToolbarModule} from '@angular/material/toolbar';
 import { NavbarComponent } from './shared/navbar/navbar.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ViewTicketComponent } from './ticket/view-ticket/view-ticket.component';
 
 @NgModule({
   declarations: [
@@ -30,7 +36,15 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatGridListModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatIconModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
